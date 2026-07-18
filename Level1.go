36c2222ebe49f3a8f9fc27e952e935889c21cea1
necanagc <scetcikovoleg@gmail.com>
2026-07-18 08:38:09 +0300

    Initial commit

---
+++ Level1.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var p1 string = "-"
+	var p2 string = "-"
+	var p3 string = "-"
+	var p4 string = "-"
+	var p5 string = "-"
+
+	var com string
+
+	var count int
+	for {
+		fmt.Scan(&com)
+
+		if com == "очередь" {
+			fmt.Printf("1. %s \n", p1)
+			fmt.Printf("2. %s \n", p2)
+			fmt.Printf("3. %s \n", p3)
+			fmt.Printf("4. %s \n", p4)
+			fmt.Printf("5. %s \n", p5)
+			continue
+		}
+
+		if com == "количество" {
+			fmt.Printf("Осталось свободных мест: %d\n", 5-count)
+			fmt.Printf("Всего человек в очереди: %d\n", count)
+			continue
+		}
+		if com == "конец" {
+			fmt.Printf("1. %s \n", p1)
+			fmt.Printf("2. %s \n", p2)
+			fmt.Printf("3. %s \n", p3)
+			fmt.Printf("4. %s \n", p4)
+			fmt.Printf("5. %s \n", p5)
+			break
+		}
+		var number int
+		fmt.Scan(&number)
+		if com == string(com) && number == int(number) {
+			if number < 1 || number > 5 {
+				fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: некорректный ввод", number))
+
+				continue
+			}
+			if count >= 5 {
+
+				fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: очередь переполнена", number))
+				continue
+			}
+			if number == 1 {
+				if p1 == "-" {
+					p1 = com
+					count += 1
+					continue
+				} else {
+
+					fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: место уже занято", number))
+					continue
+				}
+			}
+			if number == 2 {
+				if p2 == "-" {
+					p2 = com
+					count += 1
+					continue
+				} else {
+					fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: место уже занято", number))
+					continue
+				}
+			}
+
+			if number == 3 {
+				if p3 == "-" {
+					p3 = com
+					count += 1
+					continue
+				} else {
+					fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: место уже занято", number))
+					continue
+				}
+			}
+
+			if number == 4 {
+				if p4 == "-" {
+					p4 = com
+					count += 1
+					continue
+				} else {
+					fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: место уже занято", number))
+					continue
+				}
+			}
+
+			if number == 5 {
+				if p5 == "-" {
+					p5 = com
+					count += 1
+					continue
+				} else {
+					fmt.Println(fmt.Sprintf("Запись на место номер %d невозможна: место уже занято", number))
+					continue
+				}
+			}
+
+		}
+
+	}
+
+}
---
+++ Level3.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func ToLowerSlice(t []string) []string {
+	for i, r := range t {
+		t[i] = strings.ToLower(r)
+	}
+	return t
+}
+
+func findMaxWord(t map[string]int) (string, int) {
+	max := -1
+	var maxWord string
+
+	for key, _ := range t {
+		if t[key] > max {
+			max = t[key]
+			maxWord = key
+		}
+	}
+
+	return maxWord, max
+}
+
+func getTopWords(wordMap map[string]int, n int) []string {
+	var res []string
+	var maxWord string
+	for i := 0; i < n; i++ {
+		maxWord, _ = findMaxWord(wordMap)
+		res = append(res, maxWord)
+		delete(wordMap, maxWord)
+	}
+
+	return res
+}
+
+func AnalyzeText(text string) {
+
+	t := strings.FieldsFunc(text, func(r rune) bool {
+		return unicode.IsPunct(r) || unicode.IsSpace(r)
+	})
+	t = ToLowerSlice(t)
+
+	textMap := make(map[string]int)
+
+	for _, key := range t {
+		if _, ok := textMap[key]; !ok {
+			textMap[key] = 1
+		} else {
+			textMap[key]++
+		}
+	}
+
+	countUniq := len(textMap)
+
+	maxWord, maxWordCount := findMaxWord(textMap)
+
+	textMapCopy := make(map[string]int, len(textMap))
+	for k, v := range textMap {
+		textMapCopy[k] = v
+	}
+
+	top5res := getTopWords(textMapCopy, 5)
+
+	fmt.Printf("Количество слов: %v\n", len(t))
+	fmt.Printf("Количество уникальных слов: %v\n", countUniq)
+	fmt.Printf("Самое часто встречающееся слово: \"%v\" (встречается %d раз)\n", maxWord, maxWordCount)
+
+	fmt.Println("Топ-5 самых часто встречающихся слов:")
+	for _, g := range top5res {
+		fmt.Printf("\"%v\": %d раз\n", g, textMap[g])
+	}
+
+}
